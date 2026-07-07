e6f4abc596f12ffe1f0e48ad15c4a8ebc893e2b8
Lipi G Nalwathwadmath <lipign2006@gmail.com>
2026-07-07 06:46:58 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict"
+
+console.log(3+3)
+
+console.log("Hi")
+
+let name = "lipi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+console.log(typeof undefined)
+console.log(typeof null)
