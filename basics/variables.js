b6281964230b7bae2f3300eeed523a0778526738
Lipi G Nalwathwadmath <lipign2006@gmail.com>
2026-07-07 06:37:08 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountEmail = 'lipign@google.com'
+var accountPwd = '13456'
+accountCity = 'bengaluru'
+
+//accountId = 2  //not possible
+
+accountEmail = 'lipi@google.com'
+accountPwd = '70695'
+accountCity = 'Dehli'
+
+console.table([accountId,accountEmail,accountPwd,accountCity])
