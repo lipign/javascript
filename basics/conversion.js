81f9a6b993cd72e318ac83e5cace697d773ae303
Lipi G Nalwathwadmath <lipign2006@gmail.com>
2026-07-07 06:56:18 +0000

    conversions and operations

---
+++ basics/conversion.js
@@ -0,0 +1,33 @@
+let score = "lipi"
+console.log(typeof score);
+console.log(typeof(score));
+
+let number = 33
+let stringNumber = String(Number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2)
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let Count = 100
+++Count
+console.log(Count)
